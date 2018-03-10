b8b659d6dbffa75d4775e2b8e9df440529028656
Simonkyh <Simon.gribert@student.kyh.se>
2018-03-10 18:19:25 +0100

    .

--- script/calculate.js
+++ script/calculate.js
@@ -8,7 +8,9 @@ let input = document.getElementById('input'),
     },
     operators = {
       '+': function(a,b) {return a + b},
-      '-': function(a,b) {return a - b}
+      '-': function(a,b) {return a - b},
+      '*': function(a,b) {return a * b},
+      '/': function(a,b) {return a / b}
     };
 
 function display(number) {
@@ -23,12 +25,43 @@ function display(number) {
 function math(op) {
   if(cal.number1 === 0) {
     cal.number1 = Number(numbers);
+    cal.operator = op;
 } else if(cal.number1 !== 0) {
     cal.number2 = Number(numbers);
 }
-  cal.operator = op;
   cal.number1 = operators[cal.operator](cal.number1, cal.number2);
+  cal.operator = op;
   note.value = cal.number1 + " " + cal.operator + " ";
   input.value = '0';
   numbers = '0';
+}
+
+function equal() {
+  cal.number2 = Number(numbers);
+  cal.number1 = operators[cal.operator](cal.number1, cal.number2);
+  numbers = cal.number1;
+  input.value = numbers;
+  cal.number2 = 0;
+  cal.number1 = 0;
+  cal.operator = '';
+  note.value = '';
+}
+
+function clearall() {
+  input.value = '0';
+  note.value = '';
+  numbers = '0';
+  cal.number1 = 0;
+  cal.number2 = 0;
+  cal.operator = '';
+}
+
+function clearcurrent() {
+  input.value = '0';
+  numbers = input.value;
+}
+
+function clearlast() {
+  numbers = numbers.slice(0, -1)
+  input.value = numbers;
 }
