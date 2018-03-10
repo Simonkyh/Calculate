d3931e8fbde3c0c0b6f12dded6e32229b302795e
Simonkyh <Simon.gribert@student.kyh.se>
2018-03-10 21:36:19 +0100

    made all functions for standard version

--- script/calculate.js
+++ script/calculate.js
@@ -36,15 +36,44 @@ function math(op) {
   numbers = '0';
 }
 
-function equal() {
-  cal.number2 = Number(numbers);
-  cal.number1 = operators[cal.operator](cal.number1, cal.number2);
-  numbers = cal.number1;
+function percent() {
+  if(cal.number1 === 0) {
+    note.value = '0';
+    input.value = '0';
+} else {
+    numbers = (cal.number1 * Number(numbers)) / 100;
+    input.value = numbers;
+  }
+}
+
+function squareroot() {
+  numbers = Math.sqrt(Number(numbers)).toString();
   input.value = numbers;
-  cal.number2 = 0;
-  cal.number1 = 0;
-  cal.operator = '';
-  note.value = '';
+}
+
+function timesitself() {
+  numbers = numbers * numbers;
+  input.value = numbers;
+}
+
+function splitone() {
+  numbers = (1 / Number(numbers)).toString();
+  input.value = numbers;
+}
+
+function equal() {
+  if(cal.operator == '') {
+    input.value = numbers;
+} else {
+    cal.number2 = Number(numbers);
+    cal.number1 = operators[cal.operator](cal.number1, cal.number2);
+    numbers = cal.number1.toString();
+    input.value = numbers;
+    cal.number2 = 0;
+    cal.number1 = 0;
+    cal.operator = '';
+    note.value = '';
+  }
 }
 
 function clearall() {
@@ -63,5 +92,20 @@ function clearcurrent() {
 
 function clearlast() {
   numbers = numbers.slice(0, -1)
+  input.value = numbers;
+  if(numbers === '') {
+    numbers = '0';
+    input.value = numbers;
+  }
+}
+
+function posneg() {
+  if(Number(numbers) > 0) {
+    numbers = "-" + numbers;
+} else {
+    while(numbers.charAt(0) === '-') {
+      numbers = numbers.substr(1);
+    }
+  }
   input.value = numbers;
 }
